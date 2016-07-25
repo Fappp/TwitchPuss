cc3feb061ad6a7c3e77a672b45fbdc1607901430
Fabian Broekhuizen <fabian@fb-projecten.com>
2016-07-25 19:45:51 +0200

    new file:   Assets/Materials/rope.physicsMaterial2D 	new file:   Assets/Materials/rope.physicsMaterial2D.meta 	new file:   Assets/Prefabs/RopeSegment.prefab 	new file:   Assets/Prefabs/RopeSegment.prefab.meta 	modified:   Assets/Scenes/test.unity 	new file:   Assets/Scripts/ropePhys.js 	new file:   Assets/Scripts/ropePhys.js.meta 	new file:   Assets/Sprites.meta 	new file:   Assets/Sprites/Square.png 	new file:   Assets/Sprites/Square.png.meta 	new file:   Assets/testCubeMove.js 	new file:   Assets/testCubeMove.js.meta

---
+++ Assets/Scripts/ropePhys.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+
+var thickness : float = 0.2;
+var segments : int = 5;
+var segmentLength : float = 1;
+var spacing : float = 0.1;
+
+var segmentPrefab : GameObject;
+var segmentObjs : GameObject[];
+
+var pivots : Vector2[];
+
+function Start(){
+    pivots = new Vector2[ segments ];
+    segmentObjs = new GameObject[ segments ];
+    SpawnRope( segments, segmentLength );
+}
+
+function SpawnRope ( _segments : int, _segLength : float ) {
+
+    var i=0;
+
+    while ( i < _segments ){
+
+        var startPos = transform.position.y - ( i * _segLength ) - ( spacing * i );
+        pivots[i] = Vector2( transform.position.x ,startPos + ( (_segLength /2 ) + (spacing / 2) ) );
+        var clone = Instantiate( segmentPrefab, Vector3( transform.position.x, startPos, 0 ), Quaternion.identity);
+        segmentObjs[i] = clone;
+        clone.transform.localScale.x = thickness;
+        clone.transform.localScale.y = _segLength;
+        if ( i == 0 ){
+            clone.GetComponent(Rigidbody2D).isKinematic = true;
+        }
+        else{
+            clone.GetComponent(HingeJoint2D).connectedBody = segmentObjs[i -1].GetComponent(Rigidbody2D);
+        }
+        i++;
+
+    }
+
+}
+
+    function OnDrawGizmos(){
+        if ( pivots != null ){
+
+            var i=0;
+
+            while ( i < segments ){
+                Gizmos.DrawWireSphere( Vector3(pivots[i].x, pivots[i].y, 0), thickness );
+                i++;
+            }
+        }
+    }
---
+++ Assets/testCubeMove.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var speed : float = 2;
+
+var _rb : Rigidbody2D;
+
+function FixedUpdate () {
+    var hor = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+    var ver = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
+    //transform.Translate( Vector3(hor,ver,0));
+    _rb.MovePosition( _rb.position + Vector2(hor,ver) );
+}
